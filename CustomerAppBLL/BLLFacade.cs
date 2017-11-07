173d7e0cb9e253d14d79b636c15467ddbdbe640a
edijavi <edilata@msn.com>
2017-11-07 06:05:31 +0100

    fixing errors not contenplated and preparing to implement repository

--- CustomerAppBLL/BLLFacade.cs
+++ CustomerAppBLL/BLLFacade.cs
@@ -7,11 +7,6 @@ namespace CustomerAppBLL
 {
     public class BLLFacade
     {
-        public ICustumerService GetCustumerService()
-        {
-            return new CustomerService();
-        }
-
         public ICustumerService CustumerService
         {
             get { return new CustomerService(); }
