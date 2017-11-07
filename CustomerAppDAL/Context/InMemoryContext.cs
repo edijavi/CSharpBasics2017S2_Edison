344130f73bc21b97e8e434d08d09c05d4be1e546
edijavi <edilata@msn.com>
2017-11-07 15:46:27 +0100

    Creation of CustomerRepositoryEFMemory, Context folder and InMemoryContext(conexion with the db in the real example)

---
+++ CustomerAppDAL/Context/InMemoryContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomerAppDAL.Context
+{
+    class InMemoryContext : DbContext
+    {
+        static DbContextOptions<InMemoryContext> options = 
+            new DbContextOptionsBuilder<InMemoryContext>()
+            .UseInMemoryDatabase("TheDB")
+            .Options;
+
+        //Options That we want in Memory
+        public InMemoryContext() : base()
+        {
+
+        }
+    }
+}
---
+++ CustomerAppDAL/Repositories/CustomerRepositoryEFMemory.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CustomerAppEntity;
+
+namespace CustomerAppDAL.Repositories
+{
+    class CustomerRepositoryEFMemory : ICustomerRepository
+    {
+        public Customer Create(Customer cust)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Customer Delete(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Customer Get(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Customer> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
